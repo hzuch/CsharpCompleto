24ce57f52532d68eba613848ba8d6b1e15a27b22
helde <helder.zuchinalli@gmail.com>
2021-06-16 11:41:09 +0100

    45. EXERCICIO
    creates Rectangle class
    contains 2 attributes:
    * lenght
    * width
    contains 3 methods:
    * area
    * perimeter
    * diagonal

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -6,34 +6,46 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Triangulo x, y;
-            x = new Triangulo();
-            y = new Triangulo();
+            /*Produto p = new Produto();
 
-            Console.WriteLine("Entre com as medidas do triangulo X:");
-            x.A = double.Parse(Console.ReadLine());
-            x.B = double.Parse(Console.ReadLine());
-            x.C = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Entre com as medidas do triangulo Y:");
-            y.A = double.Parse(Console.ReadLine());
-            y.B = double.Parse(Console.ReadLine());
-            y.C = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
 
-            double areaX = x.Area();
-            double areaY = y.Area();
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
 
-            Console.WriteLine("Área X = " + areaX.ToString("F4"));
-            Console.WriteLine("Área Y = " + areaY.ToString("F4"));
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
 
-            if (areaX > areaY)
-            {
-                Console.WriteLine("Triângulo maior é o X");
-            }
-            else
-            {
-                Console.WriteLine("Triângulo maior é o Y");
-            }
+            Console.WriteLine();
+            Console.Write("Digite o numero de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);*/
+
+            //------------------------------------------
+            // EXERCICIO 1
+
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo");
+            r.Largura = double.Parse(Console.ReadLine());
+            r.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area = " + r.Area().ToString("F2"));
+            Console.WriteLine("perimetro = " + r.Perimetro().ToString("F2"));
+            Console.WriteLine("Diagonal = " + r.Diagonal().ToString("F2"));
         }
     }
 }
---
+++ Classes/Classes/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return Largura * 2 + Altura * 2;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
