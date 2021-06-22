6404444bb61873698a02d550abc71ca9d3942ec8
helde <helder.zuchinalli@gmail.com>
2021-06-22 20:36:20 +0100

    71. Exercise Arrays
    Creates Pensioner Class
    assign a pensioner to a room number
    prints list of occupied rooms

---
+++ Misc/ConsoleApp1/Pensioner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Pensioner
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+    }
+}
---
+++ Misc/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Pensioner[] rooms = new Pensioner[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Rent #" + (i + 1) + ":");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                rooms[roomNumber] = new Pensioner { Name = name, Email = email };
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                if (rooms[i] != null)
+                {
+                    Console.WriteLine(i + ": " + rooms[i].ToString()); 
+                }
+            }
+        }
+    }
+}
