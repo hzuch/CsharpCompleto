1a1a937980ad697835dd59707807388ecf5515e3
hldz <helder.zuchinalli@gmail.com>
2021-06-25 19:28:36 +0100

    Worker Control Exercise
    creates Class Worker
    creates Class Department
    creates Class HourContract
    creates Enum WorkerLevel
    
    set up a Worker with base salary and different contracts, then list the total income for a time period based on the contracts

---
+++ WorkerControl/WorkerControl/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿
+namespace WorkerControl.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ WorkerControl/WorkerControl/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using WorkerControl.Entities;
+using WorkerControl.Entities.Enums;
+
+namespace WorkerControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level: ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine());
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("How many contracts to worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} contract data: ");
+                Console.Write("Data DD/MM/YYYY: ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double value = double.Parse(Console.ReadLine());
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, value, hours);
+                worker.AddContract(contract);
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income: ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2"));
+        }
+    }
+}
