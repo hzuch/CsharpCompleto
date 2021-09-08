d0150a3cc71d0215bd13b3b4212e67da96e0c825
Heldz <helder.zuchinalli@gmail.com>
2021-09-08 20:48:20 +0100

    stream writer example

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,22 +7,20 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            string path = @"c:\temp\file1.txt";
+            string SourcePath = @"c:\temp\file1.txt";
+            string TargePath = @"c:\temp\file2.txt";
 
             try
             {
-                //using (FileStream fs = new FileStream(path, FileMode.Open))
-                //{
-                    //using (StreamReader sr = new StreamReader(fs))
-                    using (StreamReader sr = File.OpenText(path))
+                string[] lines = File.ReadAllLines(SourcePath);
+
+                using (StreamWriter sw = File.AppendText(TargePath))
+                {
+                    foreach (string line in lines)
                     {
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
+                        sw.WriteLine(line.ToUpper());
                     }
-                //}
+                }
             }
             catch (IOException e)
             {
