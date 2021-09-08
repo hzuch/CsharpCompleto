515868c7c4da1220dfd9cd82652f9bbba005a5e0
Heldz <helder.zuchinalli@gmail.com>
2021-09-08 20:43:45 +0100

    block using example

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -8,29 +8,26 @@ namespace Course
         static void Main(string[] args)
         {
             string path = @"c:\temp\file1.txt";
-            //FileStream fs = null;
-            StreamReader sr = null;
 
             try
             {
-                //fs = new FileStream(path, FileMode.Open);
-                //sr = new StreamReader(fs);
-                sr = File.OpenText(path);
-                while (!sr.EndOfStream)
-                {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
-                }
+                //using (FileStream fs = new FileStream(path, FileMode.Open))
+                //{
+                    //using (StreamReader sr = new StreamReader(fs))
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                //}
             }
             catch (IOException e)
             {
-                Console.WriteLine("An error occurred");
+                Console.WriteLine("error ocurred");
                 Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null) sr.Close();
-                //if (fs != null) fs.Close();
             }
         }
     }
